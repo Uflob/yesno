aac37355e4b6d75d4355a710825541b88f279cff
LaureB <lbleyer@outlook.fr>
2025-11-20 19:50:00 +0100

    maj

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@
 // app.js
 //
 import express from 'express'
+import fs from 'fs'
 
 const app = express()
+
+
+function getRandomImage() {
+   const files = fs.readdirSync('/var/www/html/yesno/img')
+   const gifs = files.filter(f => f.endsWith('.gif'))
+   const rand = Math.floor(Math.random() * gifs.length)
+   return `/img/${gifs[rand]}`
+}
+
+function getAnswerImg() {
+   const answer = Math.random() < 0.5 ? "yes" : "no"
+   const image = getRandomImage()
+   return { answer, image }
+}
 
 app.get('/', function (request, response) {
    const {answer,image} = getAnswerImg();
@@ -40,49 +55,11 @@ app.get('/', function (request, response) {
    )
 })
 
-// app.get('/about', function (request, response) {
-//    response.send(`
-//        <h2>DU Fullstack Inc.</h2>
-//        <a href='/'>Accueil</a>
-//    `)
-// })
-
-//function aleatoire () {
-//   let rand = Math.floor((Math.random)*2);
-//   if (rand === 0) { 
-//      return "Yes"
-//   } else {
-//      return "No"}
-//}
-// et dans ce cas, dans app.get, partie const data, on met "answer"=aleatoire()
-
-const imageY = ["https://media1.tenor.com/m/uqz_oXiz8usAAAAC/pedro-pascal.gif",
-   "https://media1.tenor.com/m/pYsjfAztEw8AAAAd/jack-nicholson-yes.gif"]
-const imageN = ["https://media.tenor.com/qDp2jVc3J4UAAAAi/no-love-without-love.gif","https://media1.tenor.com/m/4LMHc8wJliQAAAAd/crying-hysterically-crying-meme.gif"]
-// plutôt que de prendre des images moi-même sur le web, j'aurais pu aller sur le site officiel yesno.wtf/api, lancer ça dans mon Terminal avec npx bidule
-// copier plusieurs de leurs lignes "answer" : "no" etc.
-// les coller dans une variable ici
-
-function getAnswerImg(){
-   const rand = Math.floor((Math.random())*2)
-   const answer = (rand === 0) ? 'no' : 'yes';
-   const images = (answer === "yes") ? imageY : imageN
-   const randImage = Math.floor((Math.random())*images.length)
-   const image = images[randImage]
-
-   return {answer,image};
-}
 
 app.get('/api', function (request, response) {
-   
    response.send(getAnswerImg());
 });
 
-//app.get('/api', function (request, response) {
-//   const data = { "answer":aleatoire(),"forced":false,
-//      "image":"https://yesno.wtf/assets/yes/3-422e51268d64d78241720a7de52fe121.gif" }
-//   response.send(data)
-//})
 
 app.listen(3000, function () {
   console.log('Server listening on port 3000')
